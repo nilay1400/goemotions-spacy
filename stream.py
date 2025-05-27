bd559f9da1b3b413575d35063672c89b238f6fe9
nilay1400 <smr.nzr62@gmail.com>
2025-05-27 09:27:18 +0330

    Update model loading

--- stream.py
+++ stream.py
@@ -1,8 +1,18 @@
 import spacy
 import streamlit as st
+import os
+from huggingface_hub import snapshot_download
 
-# Load trained spaCy model
-nlp = spacy.load("output/model-best")  # adjust path if needed
+
+# Download the spaCy model from Hugging Face
+MODEL_REPO = "Nilay1400/spacy-goemotions"
+LOCAL_DIR = "./hf_model"
+
+if not os.path.exists(LOCAL_DIR):
+    snapshot_download(repo_id=MODEL_REPO, local_dir=LOCAL_DIR, local_dir_use_symlinks=False)
+
+# Load the model from the downloaded path
+nlp = spacy.load(LOCAL_DIR)
 
 st.title("GoEmotions Predictor")
 st.write("This version simulates top 5 predicted emotions for any input. Great for testing your UI!")
